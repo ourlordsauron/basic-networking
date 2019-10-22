a9aba247e62424a8e9f56a0c1fa4eb0a98a9cc1e
Derek Worth <56890071+ourlordsauron@users.noreply.github.com>
2019-10-22 13:13:37 -0500

    Add files via upload

---
+++ AverageScore.java
@@ -0,0 +1,19 @@
+//This program has a variable
+
+public class AverageScore
+{ 	
+	public static void main(String[] args)
+	{	
+	
+		int	score1 = 70; //Score #1
+		int score2 = 65; //Score #2
+		int score3 = 80; //Score #3
+		int average = (score1 + score2 + score3)/3; //Average score
+		
+		//The test scores are below
+		System.out.print("This is the average score: " + average + " out of 100");
+		
+		
+	}
+}
+	
